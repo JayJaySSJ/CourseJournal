b5745a5a3e7d5f95f2e5e14aab2c000ebaa342cc
JayJaySSJ <jakub.luczejko@gmail.com>
2022-07-04 19:39:38 +0200

    Repo created.

---
+++ CourseJournal/CourseJournal/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseJournal
+{
+    public class Class1
+    {
+    }
+}
