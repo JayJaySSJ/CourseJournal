2a98490ab22627448bc521c959522fbf0fafd829
Maria <m.paszkowska1@gmail.com>
2022-07-06 21:08:01 +0200

    Trainer 2

---
+++ CourseJournal/CourseJournal.AdminApp.Client/TrainersHandler.cs
@@ -0,0 +1,52 @@
+﻿using CourseJournal.AdminApp.Client.Clients;
+using CourseJournal.AdminApp.Client.Models;
+using System.Threading.Tasks;
+
+namespace CourseJournal.AdminApp.Client
+{
+    internal interface ITrainersHandler
+    {
+        Task<bool> CreateTrainer();
+    }
+
+    internal class TrainersHandler : ITrainersHandler
+    {
+        private readonly IConsoleManager _consoleManager;
+        private readonly ICliHelper _cliHelper;
+        private readonly ITrainerClient _trainerClient;
+
+        public TrainersHandler(IConsoleManager consoleManager, ICliHelper cliHelper, ITrainerClient trainerClient)
+        {
+            _consoleManager = consoleManager;
+            _cliHelper = cliHelper;
+            _trainerClient = trainerClient;
+        }
+
+        public async Task<bool> CreateTrainer()
+        {
+            var email = _cliHelper.GetString("Enter email");
+            bool emailExists = await _trainerClient.CheckIfExists(email);
+
+            if (emailExists)
+            {
+                _consoleManager.WriteLine($"Address {email} already exists. Choose different email address that is unique.");
+                return false;
+            }
+            else
+            {
+                Trainer newTrainer = new Trainer()
+                {
+                    Name = _cliHelper.GetString("Name"),
+                    Surname = _cliHelper.GetString("Surname"),
+                    Email = email,
+                    Password = _cliHelper.GetPassword("Password"),
+                    BirthDate = _cliHelper.GetValidDateTime("BirthDate")
+                };
+
+                _consoleManager.WriteLine($"Trainer {newTrainer.Name} {newTrainer.Surname} created successfully.");
+
+                return await _trainerClient.CreateTrainer(newTrainer);
+            }
+        }
+    }
+}
---
+++ CourseJournal/CourseJournal.Infrastructure/TrainersRepository.cs
@@ -0,0 +1,177 @@
+﻿using CourseJournal.AdminApp.Domain.Models;
+using CourseJournal.Domain;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace CourseJournal.Infrastructure
+{
+    public class TrainersRepository : ITrainersRepository
+    {
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["CourseJournalDbConnectionString"].ConnectionString;
+
+        public async Task<bool> CreateTrainer(Trainer trainer)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string commandSql = "INSERT INTO [Trainers] ([Name], [Surname], [Email], [Password], [BirthDate]) VALUES (@Name, @Surname, @Email, @Password, @BirthDate)";
+                    SqlCommand command = new SqlCommand(commandSql, connection);
+
+                    command.Parameters.Add("@Name", SqlDbType.NVarChar, 255).Value = trainer.Name;
+                    command.Parameters.Add("@Surname", SqlDbType.NVarChar, 255).Value = trainer.Surname;
+                    command.Parameters.Add("@Email", SqlDbType.NVarChar, 255).Value = trainer.Email;
+                    command.Parameters.Add("@Password", SqlDbType.NVarChar, 255).Value = trainer.Password;
+                    command.Parameters.Add("@BirthDate", SqlDbType.DateTime2).Value = trainer.BirthDate;
+
+                    if (await command.ExecuteNonQueryAsync() == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return false;
+        }
+
+        public async Task<Trainer> GetTrainer(string email)
+        {
+            Trainer trainer = null;
+
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string commandText = "SELECT * FROM [Trainers] WHERE [Email] = @Email";
+
+                    SqlCommand command = new SqlCommand(commandText, connection);
+                    command.Parameters.Add("@Email", SqlDbType.VarChar, 255).Value = email;
+
+                    SqlDataReader dataReader = await command.ExecuteReaderAsync();
+                    await dataReader.ReadAsync();
+
+                    if(dataReader.HasRows)
+                    {
+                        trainer = new Trainer
+                        {
+                            Id = int.Parse(dataReader["Id"].ToString()),
+                            Name = dataReader["Name"].ToString(),   
+                            Surname = dataReader["Surname"].ToString(),
+                            Email = email,
+                            Password = dataReader["Password"].ToString(),
+                            BirthDate = DateTime.Parse(dataReader["BirthDate"].ToString())
+                        };
+
+                        return trainer;
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                trainer = null;
+            }
+
+            return trainer;
+        }
+
+        public async Task<Trainer> GetTrainerById(int id)
+        {
+            Trainer trainer = null;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string commandText = $"SELECT * FROM [Trainers] WHERE [Id] = @Id";
+
+                    SqlCommand command = new SqlCommand(commandText, connection);
+                    command.Parameters.Add("@Id", SqlDbType.Int).Value = id;
+
+                    SqlDataReader dataReader = await command.ExecuteReaderAsync();
+
+                    await dataReader.ReadAsync();
+
+                    if (dataReader.HasRows)
+                    {
+                        trainer = new Trainer
+                        {
+                            Id = id,
+                            Name = dataReader["Name"].ToString(),
+                            Surname = dataReader["Surname"].ToString(),
+                            Email = dataReader["Email"].ToString(),
+                            Password = dataReader["Password"].ToString(),
+                            BirthDate = DateTime.Parse(dataReader["BirthDate"].ToString())
+                        };
+
+                        return trainer;
+                    }
+
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                trainer = null;
+            }
+
+            return trainer;
+        }
+
+        public async Task<List<Trainer>> GetAllAsync()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    var getAllCommandText = "SELECT * FROM [Trainers]";
+
+                    var getAllCommandSql = new SqlCommand(getAllCommandText, connection);
+                    var reader = await getAllCommandSql.ExecuteReaderAsync();
+
+                    var trainers = new List<Trainer>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        var trainer = new Trainer
+                        {
+                            Id = int.Parse(reader["Id"].ToString()),
+                            Name = reader["Name"].ToString(),
+                            Surname = reader["Surname"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            Password = reader["Password"].ToString(),
+                            BirthDate = DateTime.Parse(reader["BirthDate"].ToString())
+                        };
+
+                        trainers.Add(trainer);
+                    }
+
+                    return trainers;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                return new List<Trainer>();
+            }
+        }
+    }
+}
